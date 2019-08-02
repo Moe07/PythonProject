01fbd16ed59e769a5439834789de709f2a352850
Mohamed Abdou <abdoumohammed07@gmail.com>
2019-08-02 13:46:44 -0400

    Created database

--- mysite/blogapp/models.py
+++ mysite/blogapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title_text = models.CharField(max_length=200)
+    description_text = models.CharField(max_length=2000)
+    author_text = models.CharField(max_length=20)
+    date_created = models.DateTimeField('date created')
+     # ...
+    def __str__(self):
+        return self.title_text
+  
