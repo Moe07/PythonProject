309e070b4b6bfbb5194fb0e2aa0eb3f97fa4917d
Mohamed Abdou <abdoumohammed07@gmail.com>
2019-08-02 14:55:33 -0400

    Had to change migration to add two tables

--- mysite/blogapp/models.py
+++ mysite/blogapp/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    author_name = models.CharField(max_length=20)
+    author_bio = models.CharField(max_length=20)
+     # ...
+    def __str__(self):
+        return self.author_name
+
+
 class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title_text = models.CharField(max_length=200)
     description_text = models.CharField(max_length=2000)
-    author_text = models.CharField(max_length=20)
     date_created = models.DateTimeField('date created')
      # ...
     def __str__(self):
         return self.title_text
-  
+
+
