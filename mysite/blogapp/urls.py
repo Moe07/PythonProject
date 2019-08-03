8f3ded27c0b1a96e2ed988d92e8940bebd960fe6
Mohamed Abdou <abdoumohammed07@gmail.com>
2019-08-03 13:38:08 -0400

    User can edit post now

--- mysite/blogapp/urls.py
+++ mysite/blogapp/urls.py
@@ -5,7 +5,10 @@ from . import views
 
 urlpatterns = [
      path("", views.blog_index, name="blog_index"),
-     url(r'^add_post/$', views.add_post, name='add_post'),
      url(r'^add_post_form_submission/$', views.add_post_form_submission, name='add_post_form_submission'),
+     url(r'^edit_post_form_submission(?P<pk>\d+)/$', views.edit_post_form_submission, name='edit_post_form_submission'),
      path("<int:pk>/", views.blog_detail, name="blog_detail"),
+     path("<int:pk>", views.edit_post, name="edit_post"),
+   
+     
 ]
--- mysite/blogapp/views.py
+++ mysite/blogapp/views.py
@@ -21,8 +21,6 @@ def blog_detail(request, pk):
     }
     return render(request, "blog_detail.html", context)
 
-def add_post(request):
-    return render(request, "add_post.html")
 
 def add_post_form_submission(request):
     print("Hello form is submitted")
@@ -36,8 +34,44 @@ def add_post_form_submission(request):
     
     post = Post(title_text=title,description_text=description,date_created=timezone.now(),author=a)
     post.save()
-    
-    return render(request, "add_post.html")
+
+    posts = Post.objects.all().order_by('-date_created') 
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
                   
 
-                  
+def edit_post(request,pk):
+    post = Post.objects.get(pk=pk)
+    authors = Author.objects.filter(post=post)
+    context = {
+        "post": post,
+        "authors": authors,
+    }
+    return render(request, "edit_post.html", context)
+
+def edit_post_form_submission(request,pk):
+    
+    post = Post.objects.get(pk=pk)
+    name = request.POST["name"]
+    bio = request.POST["bio"]
+    title = request.POST["title"]
+    description = request.POST["description"]
+    a = Author(author_name=name,author_bio=bio)
+    a.save()
+    
+    post.title_text=title
+    post.description_text=description
+    post.date_created=timezone.now()
+    post.author=a
+    post.save()
+    
+    posts = Post.objects.all().order_by('-date_created') 
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+
+    
