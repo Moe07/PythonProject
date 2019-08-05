eb78df3a7595c7a9715ced40665a2162c5f38dbe
Mohamed Abdou <abdoumohammed07@gmail.com>
2019-08-04 23:08:42 -0400

    Cleaning up app

--- mysite/blogapp/urls.py
+++ mysite/blogapp/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-     path("", views.blog_index, name="blog_index"),
-     path("<int:pk>/", views.blog_detail, name="blog_detail"),
+     url(r'^$',views.blog_index, name="blog_index"),
+     url(r'^blog_detail(?P<pk>\d+)/$', views.blog_detail, name="blog_detail"),
      url(r'^add_post/$', views.add_post, name='add_post'),
      url(r'^edit_post(?P<pk>\d+)/$', views.edit_post, name="edit_post"),
      url(r'^delete_post(?P<pk>\d+)/$', views.delete_post, name="delete_post"),
